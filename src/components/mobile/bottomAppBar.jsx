5fe56f2b594b185a36b33fe309ea1605973102e7
ali-shayesteh <ali.shayesteh@gmail.com>
2023-10-29 14:01:06 +0330

    bottom appbar change

--- src/components/mobile/bottomAppBar.jsx
+++ src/components/mobile/bottomAppBar.jsx
@@ -1,11 +1,16 @@
 import { MENU } from "@/constant/config";
 import Link from "next/link";
 
+const move = function (arr, from, to) {
+    arr.splice(to, 0, arr.splice(from, 1)[0]);
+    return arr
+  };
+
 export default function BottomAppBar(){
     return (
-        <div className="bg-white h-12 shadow flex justify-between py-3 px-4 fixed bottom-0 w-full">
+        <div className="border-t bg-slate-50 flex justify-between py-3 px-10 fixed bottom-0 w-full">
             {
-            MENU.map((item, idx) => (
+            move(MENU, 0, 1).map((item, idx) => (
                 <div key={idx}>
                     <Link href={item.href}>
                         <item.icon className="w-6 h-6 text-slate-400" />
